aa0b514d6d7bf3a6ce154e03a837ca2ff85df250
Loïc Leunen <lle@apik.be>
2026-05-01 15:19:27 +0200

    cameraModal component

---
+++ src/components/CameraModal.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react'
+import { useCamera } from '../hooks/useCamera.js'
+import { useImages } from '../context/imageContext.jsx'
+import { nanoid } from 'nanoid'
+
+
+export default function CameraModal({isOpen, onClose}) {
+    const { videoRef, startCamera, stopCamera, capturePhoto } = useCamera()
+    const { setImages } = useImages()
+
+    useEffect(() => {
+        if (isOpen) {
+            startCamera() 
+        } else {
+            stopCamera()
+        }
+    }, [isOpen])
+
+    async function handleCapture() {
+        const file = await capturePhoto()
+        setImages(prevImages => [...prevImages, {
+            id: nanoid(),
+            file: file,
+            preview: URL.createObjectURL(file)
+        }])
+        onClose()
+    }
+
+    if (!isOpen) return null
+    return (
+        <div>
+            <video ref={videoRef} />
+            <button onClick={handleCapture}>Capture</button>
+            <button onClick={onClose}>Close</button>
+        </div>
+    )
+}
