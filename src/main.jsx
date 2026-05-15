6b906d3e61e8b3b13bbf86498fbaf4e2e0e7c56c
Loïc Leunen <lle@apik.be>
2026-05-15 13:42:24 +0200

    ajout du privider dans le main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ImageProvider from './context/imageContext.jsx'
+import I18nProvider from './i18n/i18nContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ImageProvider>
-        <App />
-    </ImageProvider>
+    <I18nProvider>
+        <ImageProvider>
+            <App />
+        </ImageProvider>
+    </I18nProvider>
   </StrictMode>,
 )
