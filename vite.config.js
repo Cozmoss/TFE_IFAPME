3b8a26fb5df0fd3ffeea82fe83dd1618e9e75d2a
Loïc Leunen <lle@apik.be>
2026-06-15 08:21:22 +0200

    preparation pour github pages

--- vite.config.js
+++ vite.config.js
@@ -6,50 +6,52 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-    VitePWA({
-        registerType: 'autoUpdate',
-        manifest: {
-            name: 'PixMerge',
-            short_name: 'PixMerge',
-            description: 'Merge your images into a single PDF file with ease.',
-            theme_color: '#ffffff',
-            background_color: '#ffffff',
-            display: 'standalone',
-            icons: [
-                {
-                    src: '/icons/icon-192x192.png',
-                    sizes: '192x192',
-                    type: 'image/png',
-                    purpose: 'any',
-                    media: '(prefers-color-scheme: light)'
-                },
-                {
-                    src: '/icons/icon-192x192-dark.png',
-                    sizes: '192x192',
-                    type: 'image/png',
-                    purpose: 'any',
-                    media: '(prefers-color-scheme: dark)'
-                },
-                {
-                    src: '/icons/icon-512x512.png',
-                    sizes: '512x512',
-                    type: 'image/png',
-                    purpose: 'any',
-                    media: '(prefers-color-scheme: light)'
-                },
-                {
-                    src: '/icons/icon-512x512-dark.png',
-                    sizes: '512x512',
-                    type: 'image/png',
-                    purpose: 'any',
-                    media: '(prefers-color-scheme: dark)'
-                }
-            ]
-        }
-    })
-  ],
-})
+	base: "/TFE_IFAPME/",
+	plugins: [
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+		tailwindcss(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "PixMerge",
+				short_name: "PixMerge",
+				description:
+					"Merge your images into a single PDF file with ease.",
+				theme_color: "#3b82f6",
+				background_color: "#ffffff",
+				display: "standalone",
+				icons: [
+					{
+						src: "icons/icon-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any",
+						media: "(prefers-color-scheme: light)",
+					},
+					{
+						src: "icons/icon-192x192-dark.png",
+						sizes: "192x192",
+						type: "image/png",
+						purpose: "any",
+						media: "(prefers-color-scheme: dark)",
+					},
+					{
+						src: "icons/icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+						media: "(prefers-color-scheme: light)",
+					},
+					{
+						src: "icons/icon-512x512-dark.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+						media: "(prefers-color-scheme: dark)",
+					},
+				],
+			},
+		}),
+	],
+});
